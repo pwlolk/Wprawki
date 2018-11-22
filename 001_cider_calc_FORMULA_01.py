0ac66bd5a71d167e8ce6ec2681f5ff14c8762a71
pwlolk <p.j.olszak@gmail.com>
2018-11-23 00:48:51 +0100

    Pirosiarczyn w oparciu o interpolacje w przedziłach tupla

--- 001_cider_calc_FORMULA_01.py
+++ 001_cider_calc_FORMULA_01.py
@@ -9,8 +9,6 @@ while True:
     pH_percent = float(input("Jaki procent zalecanej dawki pirosiarczynu chesz zadać[%]?: "))
     yeast_10L = float(input("Podaj ilośc drożdży [g/10L]: "))
     feed_10L = float(input("Podaj ilośc pożywki [g/10L]: "))
-    #sulphur_10L = 797883.1 + (0.2967715 - 797883.1)/(1 + (pH/18.38982)^7.846172)
-    #sulphur_10L = -0.565408051*pH**5 + 11.5743742*pH**4 - 89.3775177*pH**3 + 334.1185303*pH**2 - 611.111908*pH + 440.1496735
 
     if (pH == 2.9):
         sulphur_10L = float(tpiro[0])
