c1fd346741b6f8965a70fbc58372ffbe5e2d12fc
pwlolk <p.j.olszak@gmail.com>
2018-11-23 00:47:27 +0100

    Pirosiarczyn w oparciu o interpolacje w przedziłach tupla

--- 001_cider_calc_FORMULA_01.py
+++ 001_cider_calc_FORMULA_01.py
@@ -23,7 +23,7 @@ while True:
         i_tpH_1 = float(tpH[t_index])
         i_tpiro_0 = float(tpiro[t_index - 1])
         i_tpiro_1 = float(tpiro[t_index])
-        print('\n[Kontrolnie] Przedział pH:\t' + str(i_tpH_0) + '-' + str(i_tpH_1))
+        print('\n[Kontrolnie] Przedział pH:\t\t' + str(i_tpH_0) + '-' + str(i_tpH_1))
         print('[Kontrolnie] Przedział piro:\t' + str(i_tpiro_0) + '-' + str(i_tpiro_1))
         sulphur_10L = i_tpiro_0 + ((i_tpiro_1 - i_tpiro_0)/(i_tpH_1 - i_tpH_0))*(pH - i_tpH_0)
 
